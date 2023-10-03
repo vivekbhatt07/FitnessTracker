101dcb9e171a9f165ba1b537ca75fb21cc198c9b
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-10-03 08:57:23 +0530

    style: dashboard

--- src/Components/Cards/PrimaryCard/index.jsx
+++ src/Components/Cards/PrimaryCard/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const PrimaryCard = () => {
-  return <div></div>;
+const PrimaryCard = (props) => {
+  const { icon, value, title } = props;
+  return (
+    <div className="p-4 border flex flex-col items-center gap-2 min-w-[300px]">
+      {icon}
+      <div className="text-xl font-bold">{value}</div>
+      <div>{title}</div>
+    </div>
+  );
 };
 
 export default PrimaryCard;
--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -3,11 +3,32 @@ import { PageWrapper } from "../../Layout";
 import { PrimaryCard, LightLoader, DarkLoader } from "../../Components";
 import "./Dashboard.css";
 import { useTheme } from "../../Context/ThemeContext";
+import { Whatshot, Restaurant, SportsScore } from "@mui/icons-material";
 
 const Dashboard = () => {
   const { isDarkTheme } = useTheme();
 
-  return <PageWrapper>Dashboard</PageWrapper>;
+  const dashboardData = [
+    { id: 1, icon: <Whatshot />, value: 330, title: "Calories Burned" },
+    { id: 2, icon: <Restaurant />, value: 630, title: "Calories Consumed" },
+    { id: 3, icon: <SportsScore />, value: 5500, title: "Total Calories Goal" },
+    {
+      id: 4,
+      icon: <SportsScore />,
+      value: 5170,
+      title: "Remaining Calories to Goal",
+    },
+  ];
+
+  return (
+    <PageWrapper>
+      <section className="flex gap-4 mx-auto max-w-[1280px] pt-6 justify-between">
+        {dashboardData.map((item) => {
+          return <PrimaryCard {...item} key={item.id} />;
+        })}
+      </section>
+    </PageWrapper>
+  );
 };
 
 export default Dashboard;
